63f09003c66fdc9e5ceb685a45e28cae19eb037d
Tuomas <tuomas.orhanen@amban.fi>
2024-03-06 15:22:10 +0200

    form fixed for app router

---
+++ app/api/sendEmail/route.js
@@ -0,0 +1,37 @@
+import { NextResponse } from 'next/server';
+import nodemailer from 'nodemailer'
+
+export async function POST(request) {
+    try {
+        const { name, email, message } = await request.json();
+
+        const transporter = nodemailer.createTransport({
+          host: 'smtp.gmail.com',
+          port: 465,
+          secure: true,
+          auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+          },
+        });
+
+        const mailOption = {
+            from: process.env.EMAIL_USER,
+            to: process.env.EMAIL_RECEIVER,
+            subject: `Uusi ilmoittautuminen pienryhmään - ${name}`,
+            html: `
+        <h3>Uusi saapunut ilmoittautuminen fysiosarianne.fi lomakkeen kautta</h3>
+        <li> nimi: ${name}</li>
+        <li> sähköposti: ${email}</li>
+        <li> viesti: ${message}</li> 
+        `
+        }
+
+        await transporter.sendMail(mailOption)
+
+        return NextResponse.json({ message: "Email Sent Successfully" }, { status: 200 })
+      } catch (error) {
+        console.error(error);
+        return NextResponse.json({ message: "Failed to Send Email", error: error.message }, { status: 500 })
+    }
+}
--- components/forms/ContactForm.tsx
+++ components/forms/ContactForm.tsx
@@ -2,92 +2,120 @@
 import { useState } from 'react';
 import { track } from "@vercel/analytics";
 
+const Modal = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-center items-center">
+      <div className="bg-white text-center px-4 py-24 rounded-lg shadow-lg relative max-w-md w-full mx-2">
+        {children}
+        <button className="absolute top-0 right-0 m-4 text-2xl font-bold" onClick={onClose}>&times;</button>
+      </div>
+    </div>
+  );
+};
+
 
 export default function ContactForm({ description, title, thankYouMessage }) {
   const [loading, setLoading] = useState(false);
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  // Added state for form fields
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
 
-  async function handleSubmit(event) {
-    event.preventDefault();
+  const sendMail = async (e) => {
+    e.preventDefault();
     setLoading(true);
 
-    const data = {
-      name: String(event.target.name.value),
-      email: String(event.target.email.value),
-      message: String(event.target.message.value),
-    };
-
-    const response = await fetch('/api/contact', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    });
-
-    setLoading(false);
-    if (response.ok) {
-      setIsSubmitted(true);
+    try {
+      const response = await fetch('/api/sendEmail', {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          message,
+        })
+      });
+      const responseData = await response.json();
+      
+      if (response.ok) {
+        setIsSubmitted(true);
+        setShowModal(true); // Show thank you modal
+        setName(''); 
+        setEmail('');
+        setMessage('');
+        track('Form Submitted');
+      } else {
+        throw new Error(responseData.message || 'Something went wrong');
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
     }
   }
 
-  function handleClose() {
-    setIsSubmitted(false);
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setIsSubmitted(false); // Optionally reset isSubmitted if you want to keep the form state
   }
 
-  if (isSubmitted) {
-    return (
-      <div className="rounded-app p-4 text-center">
-        <div className="">{thankYouMessage}</div>
-      </div>
-    );
-  }
   return (
     <>
+      <Modal isOpen={showModal} onClose={handleCloseModal}>
+          <p >{thankYouMessage}</p>
+      </Modal>
       <h2 className="text-2xl font-bold text-center">{title}</h2>
       <p className='mx-auto max-w-xl text-center'>{description}</p>
-      <form onSubmit={handleSubmit} className='max-w-xl mx-auto'>
+      <form onSubmit={sendMail} className='max-w-xl mx-auto'>
         <div className="my-4 flex w-full flex-col">
-          <label className="font-bold text-gray-800" htmlFor="name"></label>
           <input
             type="text"
             placeholder="Nimi"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
             minLength={3}
             maxLength={150}
             required
-            className=" rounded-app border border-accent bg-white p-4 "
+            className="rounded-app border border-accent bg-white p-4"
             autoComplete="off"
             id="name"
           />
         </div>
         <div className="my-4 flex w-full flex-col">
-          <label className="font-bold text-gray-800" htmlFor="email"></label>
           <input
             type="email"
             placeholder="sähköposti"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             minLength={5}
             maxLength={150}
             required
-            className=" rounded-app border border-accent bg-white p-4 "
+            className="rounded-app border border-accent bg-white p-4"
             autoComplete="off"
             id="email"
           />
         </div>
         <div>
-          <label htmlFor="message"></label>
           <textarea
             rows={4}
             required
             placeholder="Viesti"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
             minLength={10}
             maxLength={500}
             name="message"
-            className="w-full rounded-app border border-accent bg-white p-4 "
+            id="message"
+            className="w-full rounded-app border border-accent bg-white p-4"
           />
         </div>
-        <button type="submit" disabled={loading} className="button mt-2 transition-scale" onClick={() => {
-                  track('Lomake lähetetty');
-                }}>
+        <button type="submit" disabled={loading} className="button mt-2 transition-scale">
           Lähetä
         </button>
       </form>
