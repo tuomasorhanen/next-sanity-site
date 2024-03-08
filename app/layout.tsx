f73d6fcab36fd84e43497229cb1d09bf58fe900b
Tuomas <tuomas.orhanen@amban.fi>
2024-03-08 15:10:24 +0200

    no tracking for preview

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({
     <html lang="fi">
       <body className={inter.className}>
         {children}
-      <Analytics mode={'production'}/>
+      <Analytics/>
       </body>
     </html>
   );
