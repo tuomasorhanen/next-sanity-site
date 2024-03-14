5c8d58164623b85a4e5ab2d8086c3f906944d7cb
Tuomas <tuomas.orhanen@amban.fi>
2024-03-14 15:56:55 +0200

    update preview to match main

--- _lib/client/client.ts
+++ _lib/client/client.ts
@@ -3,12 +3,13 @@ import { createClient } from "next-sanity";
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
 const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION
-const useCdn: boolean = !!process.env.NEXT_PUBLIC_SANITY_USE_CDN
+const token = process.env.SANITY_API_TOKEN
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  perspective: "published",
-  useCdn,
+  perspective: "previewDrafts",
+  useCdn: false,
+  token,
 });
