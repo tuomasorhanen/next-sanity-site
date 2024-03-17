17e0509b01d3b5610cfde08909f472a97489428b
Tuomas <tuomas.orhanen@amban.fi>
2024-03-17 14:55:10 +0200

    ui update for cards in blog and groups

--- components/ReferenceCards/BlogPosts.tsx
+++ components/ReferenceCards/BlogPosts.tsx
@@ -3,7 +3,13 @@ import { IRefernceItem } from '../../_lib/types/types';
 import CustomImage from '../CustomImage';
 import { Content } from '../Content';
 
+function formatDate(dateStr) {
+  const date = new Date(dateStr);
+  return new Intl.DateTimeFormat('fi-FI', { day: '2-digit', month: '2-digit', year: 'numeric' }).format(date);
+}
+
 const BlogPost = (blogPost: IRefernceItem) => {
+  const formattedDate = formatDate(blogPost.publishedAt);
   return (
     <section>
       <Link href={`/blogi/${blogPost.slug.current}`}>
@@ -14,9 +20,22 @@ const BlogPost = (blogPost: IRefernceItem) => {
             alt={blogPost.image.alt}
             className="w-full object-cover rounded-t-app"
           />
-          <div className="px-6 py-2">
+          <div className="px-4 py-2">
           <Content content={blogPost.excerpt} />
+          <div className="flex border-t border-accent mt-2 pt-2">
+              <CustomImage
+                {...blogPost.author.image}
+                width={450}
+                alt={blogPost.author.name}
+                className="mr-4 h-12 w-12 rounded-full object-cover bg-transparent shadow-app border border-accent"
+              />
+              <div className="flex flex-col text-opacity-50">
+                <p>{blogPost.author.name}</p>
+                <p>{formattedDate}</p>
+              </div>
+            </div>
           </div>
+          
         </div>
       </Link>
     </section>
